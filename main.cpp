68b1c5c3b665ebc998c17c59d5aea4a5e857188e
DmitrTRC <9984398@gmail.com>
2021-04-12 01:43:58 +0300

    transform() :  new error. Brackets

--- main.cpp
+++ main.cpp
@@ -16,19 +16,22 @@ int main() {
     unsigned long latest_user_id = 10;
     // "ws://127.0.0.1/"
     vector<thread *> threads(thread::hardware_concurrency());
-    transform(threads.begin(), threads.end(),threads.begin(),[](auto* thr){
+    transform(threads.begin(), threads.end(),threads.begin(),[](auto *thr){
+        return new thread([](){
+            uWS::App().ws<UserConnection>("/*", {
+                    .open = [&](auto *ws) {
+                        // Call on connection
+                        UserConnection *data = ws->getUserData();
+                        data->user_id = latest_user_id++;
+
+
+                    },
+                    .message = [](auto *ws, string_view message, uWS::OpCode opCode) {
+                        // Call on message
+                    }
+            });
+        });
 
     });
-    uWS::App().ws<UserConnection>("/*", {
-            .open = [&](auto *ws) {
-                // Call on connection
-                UserConnection *data = ws->getUserData();
-                data->user_id = latest_user_id++;
 
-
-            },
-            .message = [](auto *ws, string_view message, uWS::OpCode opCode) {
-                // Call on message
-            }
-    });
 }
