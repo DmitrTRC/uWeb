8bc007987a3e1c96d5bb877cbcca900461810e28
DmitrTRC <9984398@gmail.com>
2021-04-12 01:21:35 +0300

    transform() :  error.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <uwebsockets/App.h>
+#include <thread>
+#include <algorithm>
 
 using namespace std;
 
 struct UserConnection {
     unsigned long user_id;
-    string user_name;
+    string *user_name;
 
 
 };
@@ -13,6 +15,10 @@ struct UserConnection {
 int main() {
     unsigned long latest_user_id = 10;
     // "ws://127.0.0.1/"
+    vector<thread *> threads(thread::hardware_concurrency());
+    transform(threads.begin(), threads.end(),threads.begin(),[](auto* thr){
+
+    });
     uWS::App().ws<UserConnection>("/*", {
             .open = [&](auto *ws) {
                 // Call on connection
