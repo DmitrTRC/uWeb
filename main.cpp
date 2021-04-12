5dce5c2f0d49ebd5497175531c2e1a889637bb25
DmitrTRC <9984398@gmail.com>
2021-04-12 18:07:10 +0300

    Unsolved crashes problem. Changed CMake configuration

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <uwebsockets/App.h>
+#include "uwebsockets/App.h"
 #include <thread>
 #include <algorithm>
 #include <vector>
@@ -18,28 +18,31 @@ struct UserConnection {
 int main() {
     atomic_ulong latest_user_id = 10;
     // "ws://127.0.0.1/"
-    vector<thread *> threads(thread::hardware_concurrency());
-    transform(threads.begin(), threads.end(), threads.begin(), [&](auto *thr) {
-        return new thread([&]() {
-            uWS::App().ws<UserConnection>("/*", {
-                    .open = [&](auto *ws) {
-                        // Call on connection
-                        UserConnection *data = ws->getUserData();
-                        data->user_id = latest_user_id++;
-                    },
-                    .message = [](auto *ws, string_view message, uWS::OpCode opCode) {
-                        // Call on message
-                    }
-            }).listen(9999, [](auto *token) {
-                if (token) {
-                    cout << "Server started and listening on port 9999" << endl;
-                } else {
-                    cout << "Server failed to start :(" << endl;
-                }
-            }).run();
-        });
-
+//    vector<thread *> threads(thread::hardware_concurrency());
+    uWS::App().get("/hello", [](auto *res, auto *req) {
+        res->end("Hello World!");
     });
-    for_each(threads.begin(), threads.end(), [](auto *thr) { thr->join(); });
+//    transform(threads.begin(), threads.end(), threads.begin(), [&](auto *thr) {
+//        return new thread([&]() {
+//            uWS::App().ws<UserConnection>("/*", {
+//                    .open = [&](auto *ws) {
+//                        // Call on connection
+//                        UserConnection *data = ws->getUserData();
+//                        data->user_id = latest_user_id++;
+//                    },
+//                    .message = [](auto *ws, string_view message, uWS::OpCode opCode) {
+//                        // Call on message
+//                    }
+//            }).listen(9999, [](auto *token) {
+//                if (token) {
+//                    cout << "Server started and listening on port 9999" << endl;
+//                } else {
+//                    cout << "Server failed to start :(" << endl;
+//                }
+//            }).run();
+//        });
+//
+//    });
+//    for_each(threads.begin(), threads.end(), [](auto *thr) { thr->join(); });
 
 }
